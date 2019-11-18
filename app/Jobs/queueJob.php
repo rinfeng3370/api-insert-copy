6359001998c732f8241d3e379480922cb96d3f43
RinFeng <rinfeng3371@gmail.com>
2019-11-18 16:20:52 +0800

    curl移至job queueTest修改 queueTest3(整日每分鐘將資料insert入資料庫 未完成)

--- app/Jobs/queueJob.php
+++ app/Jobs/queueJob.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use DB;
+use \Curl\Curl;
+use Log;
 
 class QueueJob implements ShouldQueue
 {
@@ -18,12 +20,15 @@ class QueueJob implements ShouldQueue
      *
      * @return void
      */
-    protected $item;
+    protected $urls;
 
-    public function __construct($item)
+    protected $retry = 0;
+
+
+    public function __construct($urls)
     {
         //
-        $this->item = $item;
+        $this->urls = $urls;
     }
 
     /**
@@ -33,6 +38,51 @@ class QueueJob implements ShouldQueue
      */
     public function handle()
     {
-        DB::table('queues')->insert($this->item);
+        foreach($this->urls as $url){
+            $curl = new Curl();
+
+            echo $url. "\n";
+
+            $curl->get($url);
+
+            if ($curl->error) {   //如果發生錯誤會在重試3次 如果都失敗會把網址記到log裡
+                echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+                while ($curl->error && $this->retry < 3) {
+                    $this->retry ++;
+                    echo $this->retry. "\n";
+                    $curl->get($url);
+                }
+                if ($curl->error && $this->retry == 3){
+                    Log::warning("遺漏的單:{$url}");
+                    die();
+                }
+            }
+            
+            echo 'Response:' . "\n";
+
+            $data = json_decode($curl->response,true);
+
+            $array_key = array_keys($data);   //回傳error或沒資料時停止
+            if ($array_key[0] == "error" || $data['hits']['total']['value'] == 0) {
+                $this->error('發生錯誤或無資料!');
+                die();
+            }
+
+            foreach($data['hits']['hits'] as $i => $value){
+                $data['hits']['hits'][$i]['_source'] = stripcslashes(json_encode($value['_source']));
+                $data['hits']['hits'][$i]['sort'] = stripcslashes(json_encode($value['sort']));
+            }
+
+            $arrayLength = count($data['hits']['hits']);
+            echo "資料量: $arrayLength \n";
+            
+            $items = array_chunk($data['hits']['hits'],1000);
+
+            foreach($items as $item){
+                DB::table('queues')->insert($item);
+            }
+            
+            $curl->close();
+        }
     }
 }
